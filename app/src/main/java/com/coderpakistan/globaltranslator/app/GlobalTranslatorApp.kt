81d5dd82b3b79e92f46fe319cea4a0d9b95a6d9d
hamzaamir749 <hamzaamir749@gmail.com>
2024-12-29 15:40:14 +0000

    App Class Included

---
+++ app/src/main/java/com/coderpakistan/globaltranslator/app/GlobalTranslatorApp.kt
@@ -0,0 +1,9 @@
+package com.coderpakistan.globaltranslator.app
+
+import android.app.Application
+
+class GlobalTranslatorApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
